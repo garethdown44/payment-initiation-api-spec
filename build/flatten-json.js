182e198a31397ea08d22accb133efda81d46ff86
Jonathan Conway <jonathan@jaikoo.com>
2017-02-21 10:38:38 +0000

    WIP

---
+++ build/flatten-json.js
@@ -0,0 +1,84 @@
+const refParser = require('json-schema-ref-parser');
+const fs = require('fs');
+const path = require('path');
+const jsonPath = process.argv[2] || './schemas/v0';
+const distPath = process.argv[3] || './dist';
+const schemasRootPath = process.argv[4];
+const beautify = require('js-beautify').js_beautify;
+
+//Example usage:
+// node flatten-json.js ../opendata-api-spec/schemas/v0/ ./dist ../opendata-api-spec/
+// jsonPath: ../opendata-api-spec/schemas/v0/  location where all jsonSchemas are, which require flattening
+// distPath: location/path to write the flattened schemas to
+// schemasRootPath: root path where all schemas are present
+
+if (schemasRootPath) {
+ //runs a temporary local server in order to deference schemasRootPath
+ //needed before we can publish them publicly
+ const express = require('express');
+ const server = express();
+ server.use('/', express.static(schemasRootPath));
+ server.listen(8000);
+}
+
+fs.readdir(jsonPath, (err, filenames) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    var promises = [];
+    filenames.forEach(filename => {
+        promises.push(processFile(filename));
+    });
+    Promise.all(promises)
+        .then((res) => {
+            process.exit(0);
+        })
+        .catch((err) => {
+            process.exit(1);
+        });
+});
+
+function beautifySchema(schema) {
+    const beautifiedSchema = beautify(JSON.stringify(schema), {
+        indent_size: 2
+    });
+    return beautifiedSchema;
+}
+
+
+function writeToFile(string, filePath) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, string, (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+function processFile(filename) {
+    return new Promise((resolve, reject) => {
+        if (filename.slice(-4) !== 'json') {
+            resolve();
+            return;
+        }
+        refParser
+            .dereference(jsonPath + '/' + filename)
+            .then((flattenedSchema) => {
+                const beautifiedSchema = beautifySchema(flattenedSchema);
+                const destination = distPath + '/' + filename;
+                return writeToFile(beautifiedSchema, destination);
+            })
+            .then(() => {
+                console.log('Correctly flattened ', filename);
+                resolve();
+            })
+            .catch(err => {
+                console.log('Failed while flattening ', filename);
+                console.log(err);
+                reject(err);
+            });
+    })
+}
