1e1da30905f327e76c023e90d1e795bb0a9f5266
Claudio <claudioviolarc@gmail.com>
2017-03-16 22:31:13 +0000

    Added simple payment initial schemas and endpoints

--- build/compile-swagger.js
+++ build/compile-swagger.js
@@ -6,7 +6,6 @@ const express = require('express');
 const server = express();
 const utils = require('./utils');
 const compiled = path.resolve('../compiled');
-const exec = require('child-process-promise').exec;
 
 server.use('/', express.static(compiled));
 server.listen(8000);
@@ -16,6 +15,8 @@ process.chdir(path.dirname(swaggerFilePath));
 
 var root = YAML.load(fs.readFileSync(swaggerFilePath).toString());
 var options = {
+  //I had to put remote because this relative loader doesnt go 'up' --> ../../
+  //quick and dirty before we have a better solution
   filter        : ['relative' , 'remote'],
   loaderOptions : {
     processContent : function (res, callback) {
@@ -29,12 +30,6 @@ resolve(root, options)
     return Promise.all([
     utils.writeToFile(YAML.safeDump(results.resolved, { lineWidth: 200 }), compiled + '/swagger/rw-swagger.yaml'),
     utils.writeToFile(JSON.stringify(results.resolved, null, 2), compiled + '/swagger/rw-swagger.json')
-  ])
-})
-.then(function(res) {
-  return Promise.all([
-    exec('sed -i.bak \'/$schema/d\' \'' + compiled + '/swagger/rw-swagger.yaml\''),
-    exec('sed -i.bak \'/id:/d\' \'' + compiled + '/swagger/rw-swagger.yaml\'')
   ])
 })
 .then(function(res) {
