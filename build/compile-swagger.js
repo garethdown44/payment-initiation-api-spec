e32a62042d17b758596e24ef693455b4d270bd93
splig <craigsg@gmail.com>
2017-06-01 16:04:21 +0100

    First Draft of Payment Initiation Swagger v1.0-rc1 done

--- build/compile-swagger.js
+++ build/compile-swagger.js
@@ -2,13 +2,12 @@ const YAML = require('js-yaml');
 const SwaggerParser = require('swagger-parser');
 const utils = require('./utils');
 const path = require('path');
-const swaggerDir = './compiled/swagger';
 const compiled = path.resolve('../compiled');
 const dist = path.resolve('../dist');
-
-var swaggerIndex = path.resolve('../apis/v0/swagger/index.yaml');
+const swaggerIndex = path.resolve('../apis/v1.0/swagger/index.yaml');
 
 //https://github.com/BigstickCarpet/swagger-parser/blob/releases/4.0.0/docs/options.md
+
 const SwaggerParserOptions = {
   validate: {
     spec: true,
@@ -21,9 +20,9 @@ SwaggerParser.dereference(swaggerIndex, SwaggerParserOptions,
       console.error(err);
       throw err
     }
-    utils.writeToFile(YAML.safeDump(api, { lineWidth: 200 }), compiled + '/swagger/payment-initiation-swagger.yaml')
-    utils.writeToFile(JSON.stringify(api, null, 2), compiled + '/swagger/payment-initiation-swagger.json')
+    utils.writeToFile(YAML.safeDump(api, { lineWidth: 200 }), compiled + '/swagger/payment-initiation-swagger.yaml');
+    utils.writeToFile(JSON.stringify(api, null, 2), compiled + '/swagger/payment-initiation-swagger.json');
     // temporary....
-    utils.writeToFile(YAML.safeDump(api, { lineWidth: 200 }), dist + '/payment-initiation-swagger.yaml')
-    utils.writeToFile(JSON.stringify(api, null, 2), dist + '/payment-initiation-swagger.json')
+    utils.writeToFile(YAML.safeDump(api, { lineWidth: 200 }), dist + '/payment-initiation-swagger.yaml');
+    utils.writeToFile(JSON.stringify(api, null, 2), dist + '/payment-initiation-swagger.json');
 });
